366846aa2dee8b6e3f741c069ba2bd4916b310e4
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2025-11-04 20:26:47 +0200

    add seeders again

--- config/tenancy.php
+++ config/tenancy.php
@@ -190,7 +190,7 @@ return [
      */
     'migration_parameters' => [
         '--force' => true, // This needs to be true to run migrations in production.
-        '--path' => [database_path('migrations/tenant/testing')],
+        '--path' => [database_path('migrations/tenant')],
         '--realpath' => true,
     ],
 
